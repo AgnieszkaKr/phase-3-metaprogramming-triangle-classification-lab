3478e08d175c89b47c992a206062ba53f92fafd2
Agnes <agsawicka@gmail.com>
2022-08-11 17:50:06 -0400

    commit tree 0ad35608e209a61246a06a2edee0e707970a163f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    if @one <= 0 || @one <= 0 || @three <= 0
+      raise TriangleError 
+    elsif @one == @two && @one == @three
+      :equilateral
+    elsif @one == @two || @one == @three || @two == @three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+     "Invalid size of triangle"
+    end
+  end
+
+
 end
